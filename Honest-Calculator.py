223001586377c6c389ab5bf65d3b307311eaab7c
regiapriandi012 <regiapriandi024@gmail.com>
2022-06-19 00:49:47 +0700

    Stage 2

--- Honest-Calculator.py
+++ Honest-Calculator.py
@@ -2,6 +2,7 @@
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+msg_3 = "Yeah... division by zero. Smart move..."
 while True:
     print(msg_0)
     calc = input()
@@ -10,6 +11,20 @@ while True:
     if x.isalpha() or y.isalpha():
         print(msg_1)
     elif oper == "+" or oper == "-" or oper == "*" or oper == "/":
-        break
+        if oper == "+":
+            print(float(x) + float(y))
+            break
+        elif oper == "-":
+            print(float(x) - float(y))
+            break
+        elif oper == "*":
+            print(float(x) * float(y))
+            break
+        elif oper == "/" and float(y) != 0:
+            print(float(x) / float(y))
+            break
+        elif oper == "/" and float(y) == 0:
+            print(msg_3)
+
     else:
         print(msg_2)
