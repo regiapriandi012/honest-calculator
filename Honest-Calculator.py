991cd91f5a8595cab6497ae95da7560383169ef7
regiapriandi012 <regiapriandi024@gmail.com>
2022-06-19 00:52:21 +0700

    Stage 3

--- Honest-Calculator.py
+++ Honest-Calculator.py
@@ -3,28 +3,83 @@ msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
+
+m = 0
+result = 0
+
 while True:
     print(msg_0)
     calc = input()
 
     x, oper, y = calc.split(" ")
-    if x.isalpha() or y.isalpha():
+    if (x.isalpha() or y.isalpha()) and not (x == "M" or y == "M"):
         print(msg_1)
     elif oper == "+" or oper == "-" or oper == "*" or oper == "/":
-        if oper == "+":
-            print(float(x) + float(y))
-            break
-        elif oper == "-":
-            print(float(x) - float(y))
-            break
-        elif oper == "*":
-            print(float(x) * float(y))
-            break
-        elif oper == "/" and float(y) != 0:
-            print(float(x) / float(y))
-            break
+        if oper == "+" and not(x == "M" or y == "M"):
+            result = float(x) + float(y)
+            print(result)
+        elif oper == "+" and not (x != "M" or y == "M"):
+            result = float(m) + float(y)
+            print(result)
+        elif oper == "+" and not (x == "M" or y != "M"):
+            result = float(x) + float(m)
+            print(result)
+        #----------------------------------------
+        elif oper == "-" and not(x == "M" or y == "M"):
+            result = float(x) - float(y)
+            print(result)
+        elif oper == "-" and x == "M":
+            result = float(m) - float(y)
+            print(result)
+        elif oper == "-" and y == "M":
+            result = float(x) - float(m)
+            print(result)
+        #----------------------------------------
+        elif oper == "*" and not(x == "M" or y == "M"):
+            result = float(x) * float(y)
+            print(result)
+        elif oper == "*" and x == "M":
+            result = float(m) * float(y)
+            print(result)
+        elif oper == "*" and y == "M":
+            result = float(x) * float(m)
+            print(result)
+        #----------------------------------------
+        elif oper == "/" and x == "M":
+            result = float(m) / float(y)
+            print(result)
+        elif oper == "/" and y == "M":
+            if m == 0:
+                print(msg_3)
+                continue
+            else:
+                print(result)
+        elif (oper == "/" and float(y) != 0) and (x != "M" or y != "M"):
+            result = float(x) / float(y)
+            print(result)
+        #----------------------------------------
         elif oper == "/" and float(y) == 0:
             print(msg_3)
+            continue
+        print(msg_4)
+        answer1 = input()
+        if answer1 == "y":
+            m += result
+            print(msg_5)
+            answer2 = input()
+            if answer2 == "y":
+                continue
+            elif answer2 == "n":
+                break
+        elif answer1 == "n":
+            print(msg_5)
+            answer3 = input()
+            if answer3 == "y":
+                continue
+            elif answer3 == "n":
+                break
 
     else:
         print(msg_2)
