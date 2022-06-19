39157a6ff1584244452dfa4134e6b491e6481354
regiapriandi012 <regiapriandi024@gmail.com>
2022-06-19 16:59:54 +0700

    Stage 5 (Final)

--- Honest-Calculator.py
+++ Honest-Calculator.py
@@ -1,4 +1,5 @@
 # write your code here
+msg_ = [0,1,2,3,4,5,6,7,8,9,"Are you sure? It is only one digit! (y / n)","Don't be silly! It's just one number! Add to the memory? (y / n)","Last chance! Do you really want to embarrass yourself? (y / n)"]
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
@@ -9,6 +10,9 @@ msg_6 = " ... lazy"
 msg_7 = " ... very lazy"
 msg_8 = " ... very, very lazy"
 msg_9 = "You are"
+msg_10 = "Are you sure? It is only one digit! (y / n)"
+msg_11 = "Don't be silly! It's just one number! Add to the memory? (y / n)"
+msg_12 = "Last chance! Do you really want to embarrass yourself? (y / n)"
 
 m = 0
 result = 0
@@ -36,8 +40,10 @@ while True:
     calc = input()
 
     x, oper, y = calc.split(" ")
+
     if (x.isalpha() or y.isalpha()) and not (x == "M" or y == "M"):
         print(msg_1)
+
     elif oper == "+" or oper == "-" or oper == "*" or oper == "/":
         if oper == "+" and not(x == "M" or y == "M"):
             result = float(x) + float(y)
@@ -82,6 +88,7 @@ while True:
             print(result)
         elif oper == "*" and not(x == "M" or y == "M"):
             result = float(x) * float(y)
+            print(check(float(x), float(y), oper))
             print(result)
         elif oper == "*" and x == "M" and y == "1":
             result = float(m) * float(y)
@@ -100,11 +107,20 @@ while True:
             print(check(float(x), float(m), oper))
             print(result)
         #----------------------------------------
-        elif oper == "/" and x == "M":
+        if (oper == "/") and (x == "M" and y == "M"):
+            if x == "M":
+                x = m
+            if y == "M":
+                y = m
+            print("You are ... lazy")
+            result = float(x) / float(y)
+            print(result)
+
+        elif oper == "/" and x == "M" and not y == "M":
             result = float(m) / float(y)
             print(check(float(m), float(y), oper))
             print(result)
-        elif oper == "/" and y == "M":
+        elif oper == "/" and y == "M" and not x == "M":
             if m == 0:
                 print(check(float(x), float(m), oper))
                 print(msg_3)
@@ -122,7 +138,22 @@ while True:
         print(msg_4)
         answer1 = input()
         if answer1 == "y":
-            m += result
+            msg_index = 10
+            while is_one_digit(result):
+                print(msg_[msg_index])
+                answer4 = input()
+                if answer4 == "y":
+                    if msg_index < 12:
+                        msg_index += 1
+                    else:
+                        m += result
+                        break
+                elif answer4 == "n":
+                    msg_index += 1
+                    break
+            else:
+                m += result
+
             print(msg_5)
             answer2 = input()
             if answer2 == "y":
